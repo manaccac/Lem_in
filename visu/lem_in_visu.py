2b479f6c3d68517237ba07c4055b8702ae2b4b4f
rko <yannis.labidi666@gmail.com>
2022-05-11 12:18:22 +0200

    visu folder added

---
+++ visu/lem_in_visu.py
@@ -0,0 +1,17 @@
+
+# il faut 
+#sudo apt-get update
+#sudo apt-get install python3
+#sudo apt-get install pip3
+#pip3 install matplotlib
+#pip3 install itermplot
+import matplotlib.pyplot as plt
+import networkx as nx
+
+if __name__ == "__main__" :
+    def init():
+        file = 1;
+    __graph__ = nx.Graph();
+    init();
+
+    exit;
